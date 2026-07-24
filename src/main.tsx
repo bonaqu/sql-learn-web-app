83476f81aa8e14e7676c7d0ee361031a5954a3f7
ᴇᴜɢᴇɴᴇ <48132049+bonaqu@users.noreply.github.com>
2026-07-24 10:08:11 +0300

    Initialize unified cloud API routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { registerSW } from 'virtual:pwa-register';
+import './lib/api-fetch';
 import App from './App';
 import './styles.css';
 import './enhancements.css';
