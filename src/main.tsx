9ca95d3210b6c9d196fbca382aa97e399b5ce676
ᴇᴜɢᴇɴᴇ <48132049+bonaqu@users.noreply.github.com>
2026-07-25 20:44:12 +0300

    Keep readiness explainer inside lazy Learning Path boundary

--- src/main.tsx
+++ src/main.tsx
@@ -8,7 +8,6 @@ import ChunkErrorBoundary from './components/ChunkErrorBoundary';
 import CurriculumSyncAgent from './components/CurriculumSyncAgent';
 import DeferredFeaturePortals from './components/DeferredFeaturePortals';
 import PwaStatus from './components/PwaStatus';
-import ReadinessExplainer from './components/ReadinessExplainer';
 import './styles.css';
 import './enhancements.css';
 import './auth.css';
@@ -22,7 +21,6 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
       <AuthGate>
         <App />
         <CheckpointLauncher />
-        <ReadinessExplainer />
         <DeferredFeaturePortals />
         <CurriculumSyncAgent />
       </AuthGate>
